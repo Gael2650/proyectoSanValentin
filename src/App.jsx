1864a1724667cb938e2b4fc561ada5a166c39ae0
Gael2650 <107383492+Gael2650@users.noreply.github.com>
2024-10-21 01:18:56 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,43 +14,43 @@ function App() {
 
   let random = [{
     id: 1,
-    description: "Di si por favor",
+    description: "Estas segura?",
     img: "https://i.pinimg.com/originals/db/aa/c1/dbaac13f6278b91a15e480752b8a7242.gif"
   },
   {
     id: 1,
-    description: "Piénsalo de nuevo.",
+    description: "Completamente segura?",
     img: "https://i.pinimg.com/originals/77/6b/21/776b215bed3deeef47fd3aa657685a18.gif"
   }
     ,
   {
     id: 2,
-    description: "Vamos, atrévete a decir que sí.",
+    description: "Pero segura segura?",
     img: "https://www.gifmaniacos.es/wp-content/uploads/2019/05/gatitos-kawaii-gifmaniacos.es-19.gif"
   },
   {
     id: 3,
-    description: "No tengas miedo, será genial.",
+    description: "No mientas yo se que si",
     img: "https://i.pinimg.com/originals/e1/c3/88/e1c388133e0f998e25bb17c837b74a14.gif"
   },
   {
     id: 4,
-    description: "Confía en mí, será divertido.",
+    description: "Piénsalo de nuevo.",
     img: "https://media.tenor.com/Bn88VELdNI8AAAAi/peach-goma.gif"
   },
   {
     id: 5,
-    description: "No tengas dudas, te hará sonreír.",
+    description: "Ni poquito?",
     img: "https://i.pinimg.com/originals/c6/b3/0d/c6b30d1a2dc178aeb92de63295d4ae64.gif"
   },
   {
     id: 6,
-    description: "Te prometo que será inolvidable.",
+    description: "Por favor di que si",
     img: "https://media.tenor.com/N2oqtqaB_G0AAAAi/peach-goma.gif"
   },
   {
     id: 7,
-    description: "No dejes que el miedo te detenga.",
+    description: "Ya deja de decir que nooo",
     img: "https://i.pinimg.com/originals/db/aa/c1/dbaac13f6278b91a15e480752b8a7242.gif"
   },
   {
@@ -60,12 +60,12 @@ function App() {
   },
   {
     id: 9,
-    description: "Confía en mí.",
+    description: "No te engañes yo se que sii",
     img: "https://i.pinimg.com/originals/db/aa/c1/dbaac13f6278b91a15e480752b8a7242.gif"
   },
   {
     id: 10,
-    description: "No te arrepentirás.",
+    description: "Si pero en rojo",
     img: "https://media.tenor.com/I7KdFaMzUq4AAAAi/peach-goma.gif"
   }]
 
